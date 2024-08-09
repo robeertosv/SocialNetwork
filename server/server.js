fec8c468f511323f0fb4cf2443d985d564978b7c
robeertosv <resvolkava@gmail.com>
2024-08-09 14:46:53 +0200

    Server config

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import { configDotenv } from 'dotenv'
+import path from 'path'
+
+configDotenv()
+
+const port = process.env.PORT || 80
+const __dirname = path.resolve()
+const app = express()
+
+const corsConfig = {
+    origin: '*',
+    response: 200
+}
+
+app.use(express.json())
+app.use(cors(corsConfig))
+app.use(express.static(path.join(__dirname, '../frontend', 'dist')))
+
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname, '../frontend/dist/index.html')))
+
+app.listen(port, () => {
+    console.log('http://localhost')
+})
