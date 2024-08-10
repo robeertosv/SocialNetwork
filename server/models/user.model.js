1186f39472d4ee028c07827f542b9e4ca05789d1
robeertosv <resvolkava@gmail.com>
2024-08-10 15:46:54 +0200

    Model User

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+
+// username, name, password, isVerified, isBanned, followers, followed, posts
+const schema = mongoose.Schema({
+    username: {
+        required: true,
+        type: String
+    },
+    name: {
+        required: false,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    },
+    isVerfied: {
+        required: true,
+        type: Boolean,
+        default: false
+    },
+    isBanned: {
+        required: true,
+        type: Boolean,
+        default: false
+    },
+    // Array con los UID de los seguidores
+    followers: {
+        required: false,
+        type: Array,
+        default: []
+    },
+    // Array con los UID de los seguidos
+    following: {
+        required: false,
+        type: Array,
+        default: []
+    },
+    // Array con el postID
+    posts: {
+        required: false,
+        type: Array,
+        default: []
+    }
+})
+
+const User = mongoose.model('User', schema)
+
+export default User;
