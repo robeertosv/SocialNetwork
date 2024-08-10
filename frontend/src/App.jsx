6ee67efa816ee70a4b6b9f3ebd127b2d12d5a92e
robeertosv <resvolkava@gmail.com>
2024-08-10 19:35:04 +0200

    Create router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Settings from './pages/Settings'
+import Profile from './pages/Profile'
 
 const App = () => {
-    return (
-        <BrowserRouter>
+  return (
+    <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/settings' element={<Settings />} />
+                <Route path='*' element={<Profile/>}/>
             </Routes>
         </BrowserRouter>
-    )
+  )
 }
 
 export default App
