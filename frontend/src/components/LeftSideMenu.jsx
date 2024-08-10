bac923afaea21c8978abee24a203d1cfb819fe16
robeertosv <resvolkava@gmail.com>
2024-08-10 19:48:42 +0200

    Side menu

---
+++ frontend/src/components/LeftSideMenu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './styles/leftSideMenu.scss'
+
+const LeftSideMenu = () => {
+  return (
+    <div className='leftSideMenuContainer'>
+        <h1>Social Network</h1>
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/home">Home</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default LeftSideMenu
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import LeftSideMenu from '../components/LeftSideMenu'
+import Create from '../components/Create'
+import Feed from '../components/Feed'
+import '../styles/home.scss'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='homeContainer'>
+            <div className="homeLeft">
+                <LeftSideMenu />
+            </div>
+            <div className="homeRight">
+                <Create />
+                <Feed />
+            </div>
+        </div>
+    )
 }
 
 export default Home
